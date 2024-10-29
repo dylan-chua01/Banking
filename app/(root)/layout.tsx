d104a28f80508c9d5336f9073edf15b408c3f344
dylan-chua01 <98095414+dylan-chua01@users.noreply.github.com>
2024-10-29 16:32:41 +1100

    Implement complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,35 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+    const loggedIn = { firstName: 'Dylan', lastName: 'Chua'};
+
   return (
-    <main>
-        SIDEBAR 
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/> 
+
+        <div className="flex size-full flex-col">
+            <div className="root-layout">
+                <Image 
+                    src="/icons/logo.svg"
+                    width={30}
+                    height={30}
+                    alt="menu icon"
+                />
+                <div>
+                    <MobileNav user={loggedIn} />
+                </div>
+            </div>
+            {children}
+        </div>
+       
     </main>
   );
 }
