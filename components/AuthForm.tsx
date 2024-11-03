62a49e376af780d4cfe3442d328ffef2f307e5e9
dylan-chua01 <98095414+dylan-chua01@users.noreply.github.com>
2024-11-03 12:09:18 +1100

    Implement PlaidLink and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,6 +23,7 @@ import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 
 const AuthForm = ({ type }: { type: string }) => {
@@ -55,9 +56,23 @@ const AuthForm = ({ type }: { type: string }) => {
     setIsLoading(true);
     try {
         // Sign Up with Appwrite and create plaid link token
+        
 
         if(type === 'sign-up') {
-            const newUser = await signUp(data);
+            const userData = {
+                firstName: data.firstName!,
+                lastName: data.lastName!,
+                address1: data.address1!,
+                city: data.city!,
+                state: data.state!,
+                postalCode: data.postalCode!,
+                dateOfBirth: data.dateOfBirth!,
+                ssn: data.ssn!,
+                email: data.email,
+                password: data.password
+            }
+            
+            const newUser = await signUp(userData);
 
             setUser(newUser);
             }
@@ -110,9 +125,9 @@ const AuthForm = ({ type }: { type: string }) => {
       </header>
       {user ? (
         <div className='flex flex-col gap-4'>
-            {/* PlaidLink */}
+            <PlaidLink user={user} variant='primary'/>
         </div>
-      ) : (
+      ): (
         <>
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
